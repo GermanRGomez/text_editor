b02065421aeb9c4aa79758ffb21c19440235b3a6
GermanRGomez <grgomez@ualberta.ca>
2014-10-12 12:28:07 -0600

    Added textViewer.h file, will contain all the functions that control the GUI

---
+++ textViewer.h
@@ -0,0 +1,11 @@
+/*
+ * textViewer public interface 
+ * Set of functions to text edit control
+ */
+
+#ifndef TEXTVIEWER_H
+#define TEXTVIEWER_H
+
+// Add future viewer editing functions here
+
+#endif
