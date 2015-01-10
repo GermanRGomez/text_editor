1fb5b0bab4eaee1a0237843d47b2ddcc0e47a76d
GermanRGomez <grgomez@ualberta.ca>
2015-01-10 12:30:03 -0800

    updated all

--- textEditor.cpp
+++ textEditor.cpp
@@ -4,8 +4,16 @@
  *
  * textEditor - This class is responsible of storing the text file into 
  * memory, initialize the line buffer, manipulate the text file, and save.
- *
+ * 
+ * Based on: http://www.catch22.net/tuts/loading-text-file 
  */
 
 #include "textEditor.h"
+#include <iostream>
+#include <fstream>
 
+bool textEditor::init(char *filename)
+{
+
+  fstream file;
+  file = 
--- textEditor.h
+++ textEditor.h
@@ -1,9 +1,14 @@
 class textEditor
 {
  public:
-  
+  bool init(char *filename);
+
+  ULONG getline(ULONG lineno, char *buf, size_t len);
+  ULONG linecount();
   
  private:
-  
+  bool init_linebuffer();
+  char *buffer;
+  int length;  
 };
   
